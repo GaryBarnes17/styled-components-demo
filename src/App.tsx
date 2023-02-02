8d6ab36d4b6a0d5d2b679fdfe110b1d00bef7b82
Gary Barnes <gary.barnes@asurion.com>
2023-02-02 15:55:53 -0500

    Feedback example

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { Feedback, CheckedValue } from "@soluto-private/mx-asurion-ui-react";
-import { useState } from "react";
+import {
+  Feedback,
+  CheckedValueType,
+  CheckedValue,
+  FeedbackVariant,
+} from "@soluto-private/mx-asurion-ui-react";
+import React, { useState } from "react";
 
 const App = () => {
-  const [checked, setChecked] = useState(CheckedValue.UNSELECTED);
+  const [feedbackVal, setFeedbackVal] = useState<CheckedValueType>(
+    CheckedValue.UNSELECTED
+  );
   return (
     <>
       <Feedback
-        value={checked}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        value={feedbackVal}
+        variant={FeedbackVariant.CHECK}
+        onClick={(e: React.MouseEvent) => {
           const { value } = e.target as HTMLInputElement;
-          value === checked
-            ? setChecked(CheckedValue.UNSELECTED)
-            : setChecked(value as CheckedValue);
+          feedbackVal === value
+            ? setFeedbackVal(CheckedValue.UNSELECTED)
+            : setFeedbackVal(value as CheckedValueType);
         }}
       />
     </>
