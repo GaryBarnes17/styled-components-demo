07eb002879b16b717d72de1cf448dc4f253d6fee
Gary Barnes <gary.barnes@asurion.com>
2023-04-20 14:00:00 -0400

    textarea example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import {
-  Feedback,
-  CheckedValueType,
-  CheckedValue,
-  StarRating,
-} from "@soluto-private/mx-asurion-ui-react";
-import React, { useState } from "react";
-import styled from "styled-components";
+import { Textarea } from "@soluto-private/mx-asurion-ui-react";
+import { useState } from "react";
 
 const App = () => {
-  const [feedbackVal, setFeedbackVal] = useState<CheckedValueType>(
-    CheckedValue.UNSELECTED
-  );
+  const [formData, setFormData] = useState({});
   return (
     <>
-      <StarRating
-        color="default"
-        label="100+"
-        tooltip="See tooltip"
-        value={3.2}
-        variant="static"
+      <textarea
+        onChange={(e) =>
+          setFormData({ ...formData, textAreaOne: e.target.value })
+        }
+        value={formData?.textAreaOne}
       />
+      <textarea
+        onChange={(e) =>
+          setFormData({ ...formData, textAreaTwo: e.target.value })
+        }
+        value={formData?.textAreaTwo}
+      />
+      <Textarea
+        maxCharCount={100}
+        onChange={(e) =>
+          setFormData({ ...formData, textAreaThree: e.target.value })
+        }
+        label="Enter Some Text!"
+        value={formData?.textAreaThree}
+      />
+      <Textarea
+        onChange={(e) =>
+          setFormData({ ...formData, textAreaFour: e.target.value })
+        }
+        label="Enter Some Text!"
+        value={formData?.textAreaFour}
+      />
+      {JSON.stringify(formData)}
     </>
   );
 };
